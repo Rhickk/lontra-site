f81e1e2262e07651a521e6b3d8c193dc2fee0c88
Rhickk <rhicardo.daros@gmail.com>
2024-04-07 21:33:40 -0300

    ajustes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -195,7 +195,7 @@
   <footer class="bg-white dark:bg-gray-900">
     <div class="container flex flex-col items-center justify-between px-6 py-8 mx-auto lg:flex-row">
       <a href="#">
-        <img class=" h-14" src="./assets/logo.png" alt="">
+        <img class=" h-14" src="@/assets/logo.png" alt="">
       </a>
 
       <div class="flex flex-wrap items-center justify-center gap-4 mt-6 lg:gap-6 lg:mt-0">
